d7f6a394907b01377d62ef794d0c351ffe4c7ea7
scott robertson <scottjrobertson@gmail.com>
2021-06-28 23:04:44 +0100

    Add project files.

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+
+            Console.WriteLine("  ///////////////////////////////");
+            Console.WriteLine(" //// GUESS THE SECRET WORD ////");
+            Console.WriteLine("///////////////////////////////");
+            Console.WriteLine(" ");
+            
+
+            while(guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.WriteLine("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                } else
+                {
+                    outOfGuesses = true;
+                }
+                
+            }
+            Console.WriteLine(" ");
+            if (outOfGuesses)
+            {
+                Console.WriteLine("You ran out of guesses!");
+            } else
+            {
+                Console.WriteLine("You guessed the secret word (" + secretWord + ")!");
+            }
+            
+
+            Console.ReadLine();
+        }
+    }
+}
